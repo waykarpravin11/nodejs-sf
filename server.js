55a4351ca64d10886f723291d3d26a783d06449b
Itsmejyoti <waykar.jyoti@gmail.com>
2016-10-20 16:14:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/update', function(req, res) {
+app.post('/insert', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            'INSERT INTO salesforce.Personal_Information__c Phone__c = $1, Email__c=$4,First_Name__c=$2,Last_Name__c=$3',
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  conn.query('INSERT INTO salesforce.Contact (Phone__c, First_Name__c, Last_Name__c,Email__c) VALUES ($1, $2, $3, $4)',
+                  [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
                   function(err, result) {
                     done();
                     if (err) {
